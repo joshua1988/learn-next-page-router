77b40347d34fba003ffcb1701b6d54de6d289fe3
joshua1988 <jangkeehyo@gmail.com>
2024-04-12 23:21:27 +0900

    상품 목록 컴포넌트화

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { fetchProducts } from '@/api';
+import React, { useEffect, useState } from 'react';
 
 function ProductList() {
-	return <div>ProductList</div>;
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		fetchProducts().then(res => {
+			setProducts(res.data);
+		});
+	}, []);
+
+	return (
+		<div>
+			<ul>
+				{products &&
+					products.map((product, index) => {
+						return <li key={index}>{product.name}</li>;
+					})}
+			</ul>
+		</div>
+	);
 }
 
 export default ProductList;
--- pages/products.jsx
+++ pages/products.jsx
@@ -1,27 +1,11 @@
-import { fetchProducts } from '@/api';
+import ProductList from '@/components/ProductList';
 import BasicLayout from '@/layouts/BasicLayout';
-import { useEffect, useState } from 'react';
 
 function ProductPage() {
-	const [products, setProducts] = useState([]);
-
-	useEffect(() => {
-		fetchProducts().then(res => {
-			setProducts(res.data);
-		});
-	});
-
 	return (
 		<BasicLayout>
 			<h1>상품 목록</h1>
-			<div>
-				<ul>
-					{products &&
-						products.map((product, index) => {
-							return <li key={index}>{product.name}</li>;
-						})}
-				</ul>
-			</div>
+			<ProductList></ProductList>
 		</BasicLayout>
 	);
 }
