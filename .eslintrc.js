47e318f15fdbb848d4644a618e269d114ae5eeab
joshua1988 <jangkeehyo@gmail.com>
2024-04-12 22:21:41 +0900

    configure eslint and prettier with fixing code

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			// 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+			// https://prettier.io/docs/en/options.html
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+	},
+};
