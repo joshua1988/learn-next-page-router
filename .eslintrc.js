85c225385bf91f0272bfb6b5e9f46a67c1c80981
joshua1988 <jangkeehyo@gmail.com>
2024-04-12 23:21:16 +0900

    선언되지 않은 변수, 임포트 구문에 대한 린트 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,11 @@ module.exports = {
 	extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
 	plugins: ['prettier', 'unused-imports'],
 	rules: {
+		// 선언되지 않은 변수 또는 임포트 구문 정리 규칙
+		'no-undef': 'error',
 		'unused-imports/no-unused-imports': 'error',
+
+		// 프리티어 설정
 		'prettier/prettier': [
 			'error',
 			// 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
