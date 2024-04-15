7245e0ac66907055d717b364d259c3fc099e5a54
joshua1988 <jangkeehyo@gmail.com>
2024-04-16 08:26:28 +0900

    상품 상세 스타일 및 이미지 태그 변경

--- components/ProductDetail.jsx
+++ components/ProductDetail.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from './ProductDetail.module.css';
 import { createCartItem } from '@/api';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 function ProductDetail({ product }) {
 	const { imageUrl, price, name, id } = product;
+	const router = useRouter();
+
 	const addCart = async () => {
 		try {
 			const { data } = await createCartItem(product);
 			alert(`${data.name}가 장바구니에 담겼습니다`);
+			router.push('/cart');
 		} catch (error) {
 			console.log(error);
 		}
@@ -18,7 +23,7 @@ function ProductDetail({ product }) {
 			<h1>상품 상세 페이지</h1>
 			<div className={styles.container}>
 				<div>
-					<img src={imageUrl} alt={name} />
+					<Image src={imageUrl} width={250} height={250} alt={name} />
 				</div>
 				<div className={styles.description}>
 					<p>{name}</p>
