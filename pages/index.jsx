01c6183a6f654505015067855a256985351370cf
joshua1988 <jangkeehyo@gmail.com>
2024-04-27 16:27:59 +0900

    카운터 컴포넌트 제작

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,26 @@
-import ProductList from '@/components/ProductList';
-import BasicLayout from '@/layouts/BasicLayout';
+import { useState } from 'react';
+
+function Counter() {
+	const [counter, setCounter] = useState(0);
+
+	const addCounter = () => setCounter(counter + 1);
+	const substractCounter = () => setCounter(counter - 1);
+
+	return (
+		<div>
+			<p>{counter}</p>
+			<button onClick={addCounter}>+</button>
+			<button onClick={substractCounter}>-</button>
+		</div>
+	);
+}
 
 function ProductPage() {
-	return (
-		<BasicLayout>
-			<h1>상품 목록</h1>
-			<ProductList></ProductList>
-		</BasicLayout>
-	);
+	return <Counter></Counter>;
+	// <BasicLayout>
+	// 	<h1>상품 목록</h1>
+	// 	<ProductList></ProductList>
+	// </BasicLayout>
 }
 
 export default ProductPage;
