e4c111a1b7394a2c51010a0783817f675379c501
joshua1988 <jangkeehyo@gmail.com>
2024-04-28 13:46:46 +0900

    싸이프레스 ESLint 플러그인 설치 및 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-	extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
-	plugins: ['prettier', 'unused-imports'],
+	extends: [
+		'next/core-web-vitals',
+		'plugin:prettier/recommended',
+		'plugin:cypress/recommended',
+	],
+	plugins: ['prettier', 'unused-imports', 'cypress'],
 	rules: {
 		// 선언되지 않은 변수 또는 임포트 구문 정리 규칙
 		'no-undef': 'error',
--- cypress/e2e/home-page.cy.js
+++ cypress/e2e/home-page.cy.js
@@ -1,12 +1,7 @@
-// TODO: cypress eslint 플러그인 설치
-// eslint-disable-next-line no-undef
 describe('카운터 앱', () => {
-	// eslint-disable-next-line no-undef
 	it('인덱스 페이지에 진입하면 카운터가 0으로 설정되어 있다.', () => {
-		// eslint-disable-next-line no-undef
 		cy.visit('http://localhost:3000');
 
-		// eslint-disable-next-line no-undef
 		cy.get('[data-cy=count]').contains('0');
 	});
 });
