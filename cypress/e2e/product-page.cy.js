1f8536ccc91b831bf722e7e27720c32c278f50c3
joshua1988 <jangkeehyo@gmail.com>
2024-05-04 14:20:57 +0900

    상품 목록 페이지 테스트 코드의 공통 훅 분리

--- cypress/e2e/product-page.cy.js
+++ cypress/e2e/product-page.cy.js
@@ -1,18 +1,19 @@
 describe('상품 목록 페이지', () => {
-	it('페이지에 접속하면 상품 목록이 표시된다.', () => {
+	beforeEach(() => {
 		cy.visit('/');
+	});
+
+	it('페이지에 접속하면 상품 목록이 표시된다.', () => {
 		// cy.get('a').should('be.visible')
 		cy.getByCy('product').should('be.visible');
 	});
 
 	it('페이지 상단의 Cart 메뉴를 클릭하면 장바구니 페이지로 이동한다.', () => {
-		cy.visit('/');
 		cy.getByCy('cart-menu').click('');
 		cy.getByCy('cart-header').should('be.visible');
 	});
 
 	it('상품을 클릭하면 상품 상세 페이지로 이동한다.', () => {
-		cy.visit('/');
 		cy.getByCy('product').first().click();
 		cy.url().should('include', '/products/');
 	});
